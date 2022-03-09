ed1fa5d55066e7f78925cb7e8bf050721d221abb
Winston <winstonlimhs@gmail.com>
2022-03-09 23:32:38 +0800

    make url check static

--- cli.py
+++ cli.py
@@ -5,9 +5,13 @@ class Cli:
     url : str
     def prompt_user(self):
         perhaps_a_link = input('please enter the property guru link:')
+        Cli.check_url(perhaps_a_link)
+
+    @staticmethod
+    def check_url(url: str):
         validate = URLValidator()
         try:
-            validate(perhaps_a_link)
+            validate(url)
             print("Entered a valid URL.")
         except ValidationError as e:
             print("Entered an invalid URL")
