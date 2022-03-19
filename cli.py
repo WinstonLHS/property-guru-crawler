147edc6860cce88b4ea39c4e70d88c37d314528f
Winston <winstonlimhs@gmail.com>
2022-03-20 01:06:31 +0800

    rename webcrawler to webscraper

--- cli.py
+++ cli.py
@@ -1,8 +1,8 @@
-from webcrawler import fetch_html
-from webcrawler import save_to_file
-from webcrawler import find
-from webcrawler import prompt_for_url
-from webcrawler import extract_property
+from webscraper import fetch_html
+from webscraper import save_to_file
+from webscraper import find
+from webscraper import prompt_for_url
+from webscraper import extract_property
 
 class Cli:
     url : str
